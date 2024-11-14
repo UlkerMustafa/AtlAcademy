83f1552f4cf664f7cb8819063ca1b9a08140a1d6
Ulka <ulkar.mustafazada@gmail.com>
2024-11-14 21:45:16 +0400

    task1

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Lesson4 {
+public class Lesson4{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ededi daxil edin: ");
+        int eded = scanner.nextInt();
+        int reqemSayi = String.valueOf(Math.abs(eded)).length();
+
+        System.out.println(eded + " " + reqemSayi + " rəqəmli ədəddir.");
+
+
+    }
 }
