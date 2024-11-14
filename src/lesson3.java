bb95756e89c7739b2f13952117ada7996409e218
Ulka <ulkar.mustafazada@gmail.com>
2024-11-14 15:03:39 +0400

    tyui

--- src/lesson3.java
+++ src/lesson3.java
@@ -1,19 +1,50 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class lesson3 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Istifadeci adi: ");
 
-         var username= "admin";
-         var name = scanner.next();
-         if (name.equals(username)){
-             System.out.println("yes");
-         }
-         else {
-             System.out.println("no");
-         }
+
+
+
+
+
+
+
+
+//        int[] box = {1, 4, 7};
+//        for (var i = box.length-1; i >= 0; i--) {
+//            System.out.println(box[i]);
+//        }
+
+        //  System.out.println(Arrays.toString(box));
+        // System.out.println(box[3]);  //nomreleme 0 baslayor
+        //  System.out.println(box.length); //uzunlugu6
+//for (var i=0; i< box.length;i++){
+//    System.out.println(box[i]);
+//}
+
+//        for(var a=1;a<=9;a++){
+//            for(var b=10;b>=1;b--){
+//                System.out.println(a+"."+b);
+//            }
+
+
+//        for(var a=1;a<=9;a++){
+//            for(var b=1;b<=10;b++){
+//                System.out.println(a + "*"+ b+ "="+ a*b);
+//            }
+//        }
+
+
+//        System.out.println("birinci eded daxil edin");
+//        int a = scanner.nextInt();
+//
+//      for (var b=1; b<=10; b++ ){
+//          System.out.println(b + "*"+ a+"="+ a*b);}
+
 
     }
 }
+
