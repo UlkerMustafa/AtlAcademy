9ce6caa1fe04f29a0fcdbb1dbcf8003bf50cf221
Ulka <ulkar.mustafazada@gmail.com>
2024-11-16 12:00:13 +0400

    task1

---
+++ src/tasklar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class tasklar {
+    public static void main(String[] args) {
+       int cem=0;
+       int a=1;
+       do {
+           cem += a;
+           a++;
+       }while (a<=10);
+        System.out.println(cem);
+
+
+    }
+}
