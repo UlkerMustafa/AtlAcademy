e27cdfd0ba65554f3d2d4362b17901276b4b60ba
Ulka <ulkar.mustafazada@gmail.com>
2024-11-18 00:20:55 +0400

    task1

---
+++ src/array1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        //array elementi  daxil edirem
+        System.out.println("Array elementlərini daxil edin:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        //array elementlerini bir setirde cap et
+        System.out.print("array elementlerini bir setirde cap et: ");
+        for (int i=0;i<array.length;i++)
+        {
+            System.out.print(array[i]+ "  ");
+        }
+
+    }}
+
