ccf88f93f9ebd9e00a2636009ede1f0a1f69f70c
Ulka <ulkar.mustafazada@gmail.com>
2024-12-20 13:21:27 +0400

    task1

---
+++ src/Stream/Stream4.java
@@ -0,0 +1,24 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream4 {
+    public static void main(String[] args) {
+
+
+        List<List<Integer>> myList= Arrays.asList(
+                Arrays.asList(12,23,34),
+                Arrays.asList(45,56,78),
+                Arrays.asList(33,567,777)
+
+        );
+        myList.stream()
+                .map(s->s.stream().toList())
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+
+    }
+}
